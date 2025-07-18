7c8d50f62d67dae388b35f3021a2bc385d2f2b1e
Franco Grobler <franco@grobler.fyi>
2025-07-18 12:14:54 +0200

    fix(nvim): use locally installed lsps

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,19 +1,19 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = { eslint = {} },
-      setup = {
-        eslint = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
-      },
-    },
+    opts = function(_, opts)
+      local ensure_installed = {}
+      for server, server_opts in pairs(opts.servers) do
+        if type(server_opts) == "table" and not ensure_installed[server] then
+          server_opts.mason = false
+        end
+      end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = { "markdown-toc" }
+    end,
   },
 }
