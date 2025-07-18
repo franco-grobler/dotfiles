e57824a2c7057d6758678c3b1b460f98073e3737
Franco Grobler <franco@grobler.fyi>
2025-07-18 10:18:06 +0200

    fix(nix): set locals

--- nix/users/francogrobler/nixos.nix
+++ nix/users/francogrobler/nixos.nix
@@ -3,6 +3,19 @@
   # Add ~/.local/bin to PATH
   environment.localBinInPath = true;
 
+  i18n = {
+    defaultLocale = "en_ZA.UTF-8";
+
+    extraLocales = [
+      "en_GB.UTF-8/UTF-8"
+    ];
+
+    extraLocaleSettings = {
+      LC_CTYPE = "en_ZA.UTF-8";
+      LC_ALL = "en_ZA.UTF-8";
+    };
+  };
+
   programs.zsh.enable = true;
 
   users.users.francogrobler = {
