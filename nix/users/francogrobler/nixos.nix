69bdc1c9fb73e8ee1cd5a96f81c516fee23562b8
Franco Grobler <franco@grobler.fyi>
2025-07-18 15:37:00 +0200

    feat(nix): use nix ld for some binarie

--- nix/users/francogrobler/nixos.nix
+++ nix/users/francogrobler/nixos.nix
@@ -1,19 +1,21 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   # Add ~/.local/bin to PATH
   environment.localBinInPath = true;
 
   i18n = {
     defaultLocale = "en_ZA.UTF-8";
 
-    extraLocales = [
-      "en_GB.UTF-8/UTF-8"
-    ];
+    extraLocales = [ "en_GB.UTF-8/UTF-8" ];
 
     extraLocaleSettings = {
       LC_CTYPE = "en_ZA.UTF-8";
       LC_ALL = "en_ZA.UTF-8";
     };
+  };
+
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [ markdown-toc ruff ];
   };
 
   programs.zsh.enable = true;
