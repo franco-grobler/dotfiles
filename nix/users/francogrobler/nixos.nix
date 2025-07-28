ff2dbd413a7c00a83a26970141bb9fe18e96cf84
Franco Grobler <franco@grobler.fyi>
2025-07-28 13:45:34 +0200

    feat(nix): add podman support

--- nix/users/francogrobler/nixos.nix
+++ nix/users/francogrobler/nixos.nix
@@ -32,4 +32,20 @@
   };
 
   time.timeZone = "Africa/Johannesburg";
+
+  virtualisation = {
+    containers = {
+      enable = true;
+      containersConf = { settings = { compose_warning_logs = false; }; };
+    };
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
 }
