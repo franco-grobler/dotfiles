7eddc8824abdf6e9c9e89f5b1e4c2d47ec55b12a
Franco Grobler <franco@grobler.fyi>
2025-08-07 09:58:33 +0200

    feat(nvim): set vimtext viewer based on operating system

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local os = vim.loop.os_uname().sysname
+local is_mac = os == "Darwin"
+
 local opt = vim.opt
 
 opt.wrap = true  -- Enable line wrap
@@ -14,7 +17,11 @@ local g = vim.g
 g.lazyvim_python_lsp = "mypy"
 
 -- VimTex
-g.vimtex_view_method = "skim"
+if is_mac then
+  g.vimtex_view_method = "skim"
+else
+  g.vimtex_view_method = "zathura"
+end
 
 -- File types
 local ft = vim.filetype
