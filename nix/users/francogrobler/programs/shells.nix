273cadbc9289b579b60015148f42b8e2381e550d
Franco Grobler <franco@grobler.fyi>
2025-11-15 12:06:19 +0200

    chore(nix): format shells
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nix/users/francogrobler/programs/shells.nix
+++ nix/users/francogrobler/programs/shells.nix
@@ -1,39 +1,41 @@
 { shellAliases }:
 {
-  programs.bash = {
-    enable = true;
-    shellOptions = [ ];
-    historyControl = [
-      "ignoredups"
-      "ignorespace"
-    ];
-    shellAliases = shellAliases;
-  };
-
-  programs.nushell = {
-    enable = true;
-    shellAliases = shellAliases;
-  };
-
-  programs.zsh = {
-    enable = true;
-    autosuggestion = {
+  programs = {
+    bash = {
       enable = true;
+      shellOptions = [ ];
+      historyControl = [
+        "ignoredups"
+        "ignorespace"
+      ];
+      inherit shellAliases;
     };
-    defaultKeymap = "vicmd";
-    initContent = ''
-      # Nix
-      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-      fi
-      # End Nix
 
-      # Dotfiles
-      source "$HOME/.config/zsh/zshrc"
-    '';
-    shellAliases = shellAliases;
-    syntaxHighlighting = {
+    nushell = {
       enable = true;
+      inherit shellAliases;
+    };
+
+    zsh = {
+      enable = true;
+      autosuggestion = {
+        enable = true;
+      };
+      defaultKeymap = "vicmd";
+      initContent = ''
+        # Nix
+        if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+          . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+        fi
+        # End Nix
+
+        # Dotfiles
+        source "$HOME/.config/zsh/zshrc"
+      '';
+      inherit shellAliases;
+      syntaxHighlighting = {
+        enable = true;
+      };
     };
   };
 }
