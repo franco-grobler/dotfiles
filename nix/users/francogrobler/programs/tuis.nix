2bbb104d5e222db5417439325d88866a1901179a
Franco Grobler <franco@grobler.fyi>
2025-11-15 12:20:41 +0200

    feat(nix): add lazysql
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nix/users/francogrobler/programs/tuis.nix
+++ nix/users/francogrobler/programs/tuis.nix
@@ -2,9 +2,10 @@
   programs = {
     lazygit = {
       enable = true;
-      enableBashIntegration = true;
-      enableNushellIntegration = true;
-      enableZshIntegration = true;
+    };
+
+    lazysql = {
+      enable = true;
     };
   };
 }
