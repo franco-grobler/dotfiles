cddbb1991dd2e70a2f4d8393df0a11eed7a4a826
Franco Grobler <franco@grobler.fyi>
2025-12-02 18:04:39 +0200

    feat(nix): bump nix version
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,16 +3,16 @@
 
   inputs = {
     # Default to stable, use unstable for some packages
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
