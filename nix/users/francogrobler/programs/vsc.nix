fd52c8d34ad003f243f93e3cb47993bc14f8568f
Franco Grobler <franco@grobler.fyi>
2025-12-03 21:57:07 +0200

    fix(nix): update config for new release

--- nix/users/francogrobler/programs/vsc.nix
+++ nix/users/francogrobler/programs/vsc.nix
@@ -41,7 +41,7 @@ in
         helper = "store"; # want to make this more secure
       };
       github = {
-        user = "franco-from-gcc";
+        user = "franco-grobler";
       };
       gpg = {
         format = "ssh";
@@ -56,6 +56,11 @@ in
         default = "tracking";
       };
       user = {
+        aliases = {
+          cleanup = "!git branch --merged | grep  -v '\\*\\|main\\|develop' | xargs -n 1 -r git branch -d";
+          prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+          root = "rev-parse --show-toplevel";
+        };
         email = "franco@grobler.fyi";
         name = "Franco Grobler";
         signingKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDSIiPwJz9rAYiMlbrbPEqGFyxYIwf4oi7QNENCJPHWCaFWH2Y54PAVh1dt0FTQaDlY92e+fi/QA/QbJrAfoNuuhwVvP/FpZ8a4ihdljhgpBQZpiSTBa6xnA0QMd0fAOQywcazDoMYRaSMpFoOLvxXCZ+W/eoPeifaOQdNk8zs8RmXXj155nLu3hFJ2lEj2ouCuP0jkCh+k0QeoOjVumsSr1CQWn/TIb9kt1msmlWO0/2CTaMT4+q5uAuuDWxB8V2TcjINeOoDGJnkG79Q3N/jtXV09Mstt6W5qP+x62Rod/eZ+gZYVcGYxeLAFj3eTw6neEup1aLI57UbDkGRVzDAw5/KNuhWrtP6ex/V+ZhQUkU8QiQXIbLzWXt351o4G9a5FBncywLob8YLWM4O1OITlz50ciUeWytUNXIuqMlVvzyueJ4c2LCz8KArNU9avhz0F2whBzDMOlWWDOQGS90OIPfxOtHlUx+YY7oFSpdFqZkwu5Dc1+CvNkCM8oSFlLpc=";
