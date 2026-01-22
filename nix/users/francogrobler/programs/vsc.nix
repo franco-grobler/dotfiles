463a77dd995b1a7c7ed04c51ea13acdad02e7acb
Franco Grobler <franco@grobler.fyi>
2026-01-22 09:30:21 +0200

    feat(nix): update git signing key

--- nix/users/francogrobler/programs/vsc.nix
+++ nix/users/francogrobler/programs/vsc.nix
@@ -24,7 +24,7 @@ in
           user = {
             email = "franco.grobler@lexisnexis.co.za";
             name = "Franco Grobler";
-            signingKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDSIiPwJz9rAYiMlbrbPEqGFyxYIwf4oi7QNENCJPHWCaFWH2Y54PAVh1dt0FTQaDlY92e+fi/QA/QbJrAfoNuuhwVvP/FpZ8a4ihdljhgpBQZpiSTBa6xnA0QMd0fAOQywcazDoMYRaSMpFoOLvxXCZ+W/eoPeifaOQdNk8zs8RmXXj155nLu3hFJ2lEj2ouCuP0jkCh+k0QeoOjVumsSr1CQWn/TIb9kt1msmlWO0/2CTaMT4+q5uAuuDWxB8V2TcjINeOoDGJnkG79Q3N/jtXV09Mstt6W5qP+x62Rod/eZ+gZYVcGYxeLAFj3eTw6neEup1aLI57UbDkGRVzDAw5/KNuhWrtP6ex/V+ZhQUkU8QiQXIbLzWXt351o4G9a5FBncywLob8YLWM4O1OITlz50ciUeWytUNXIuqMlVvzyueJ4c2LCz8KArNU9avhz0F2whBzDMOlWWDOQGS90OIPfxOtHlUx+YY7oFSpdFqZkwu5Dc1+CvNkCM8oSFlLpc=";
+            signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICeVs/cj+69xAUIX3CLGK9HSI6ChyQ2JvE0tFYAb9Gbd";
           };
         };
       }
