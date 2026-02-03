e05236ab49948c52ed12c6262ddfb9a47b95d616
Franco Grobler <franco@grobler.fyi>
2026-02-03 20:57:17 +0200

    feat(nix): add posting to installed apps
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nix/flake.nix
+++ nix/flake.nix
@@ -48,6 +48,7 @@
             gh
             nushell
             opencode
+            posting
             uv
             ;
         })
--- nix/users/francogrobler/home-manager.nix
+++ nix/users/francogrobler/home-manager.nix
@@ -105,6 +105,7 @@ in
         podman
         podman-compose
         podman-tui
+        posting
         python314
         qmk
         ripgrep
