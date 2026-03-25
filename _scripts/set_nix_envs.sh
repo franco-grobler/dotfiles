fc0e1b8072aa78bfb85814870960327d117bd817
Franco Grobler <franco@grobler.fyi>
2026-03-25 16:09:48 +0200

    chore: update nix envs script formatting

--- _scripts/set_nix_envs.sh
+++ _scripts/set_nix_envs.sh
@@ -2,35 +2,35 @@
 set -euo pipefail
 
 is_nixos() {
-	# Check if /etc/NixOS/release file exists
-	if [ -f "/etc/NixOS/release" ]; then
-		return 0
-	fi
+  # Check if /etc/NixOS/release file exists
+  if [ -f "/etc/NixOS/release" ]; then
+    return 0
+  fi
 
-	# Check if the nixos-version command exists and is executable
-	if command -v nixos-version &>/dev/null; then
-		return 0
-	fi
+  # Check if the nixos-version command exists and is executable
+  if command -v nixos-version &>/dev/null; then
+    return 0
+  fi
 
-	# If neither condition is met, it's likely not NixOS
-	return 1
+  # If neither condition is met, it's likely not NixOS
+  return 1
 }
 
-os=$(uname -s)
-if [[ $os == "Darwin" ]]; then
-	export NIXNAME="apple-silicone"
-	export NIXCONFIG="darwinConfigurations"
-elif [[ $os == "Linux" ]]; then
-	export NIXNAME="x86_64-linux"
-	if is_nixos; then
-		export NIXCONFIG="nixosConfigurations"
-	else
-		export NIXCONFIG="homeConfigurations"
-	fi
-	if [[ $(uname -r) =~ WSL ]]; then
-		export NIXNAME="wsl"
-	fi
+os="$(uname -s)"
+if [[ "$os" == "Darwin" ]]; then
+  export NIXNAME="apple-silicone"
+  export NIXCONFIG="darwinConfigurations"
+elif [[ "$os" == "Linux" ]]; then
+  export NIXNAME="x86_64-linux"
+  if is_nixos; then
+    export NIXCONFIG="nixosConfigurations"
+  else
+    export NIXCONFIG="homeConfigurations"
+  fi
+  if [[ "$(uname -r)" =~ "WSL" ]]; then
+    export NIXNAME="wsl"
+  fi
 else
-	echo "Unsupported OS: $os"
-	exit 1
+  echo "Unsupported OS: $os"
+  exit 1
 fi
