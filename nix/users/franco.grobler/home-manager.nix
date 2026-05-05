086da6a162652d5cbd061b7f3fd5accd807c6a49
Franco Grobler <franco@grobler.fyi>
2026-05-05 11:39:23 +0200

    fix(nix): xdg config path reading
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nix/users/franco.grobler/home-manager.nix
+++ nix/users/franco.grobler/home-manager.nix
@@ -160,8 +160,8 @@ in
       PAGER = "less -FirSwX";
       PODMAN_COMPOSE_WARNING_LOGS = "false";
 
-      BAT_CONFIG_PATH = "$XDG_CONFIG_HOME/bat/config";
-      CURSOR_CONFIG_DIR = "$XDG_CONFIG_HOME/cursor";
+      BAT_CONFIG_PATH = "${config.xdg.configHome}/bat/config";
+      CURSOR_CONFIG_DIR = "${config.xdg.configHome}/cursor";
 
       GEMINI_API_KEY = "op://Personal/Gemini CLI/credential";
     }
