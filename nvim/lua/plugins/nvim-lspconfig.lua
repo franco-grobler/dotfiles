88e8d821e83bfe433ac13374444980fbf05d979f
Franco Grobler <franco@grobler.fyi>
2026-07-27 18:17:50 +0200

    feat(nvim): typst lsp settings
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -44,7 +44,35 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      servers = { eslint = {} },
+      servers = {
+        eslint = {},
+        tinymist = {
+          keys = {
+            {
+              "<leader>cP",
+              function()
+                local buf_name = vim.api.nvim_buf_get_name(0)
+                local file_name = vim.fn.fnamemodify(buf_name, ":t")
+                LazyVim.lsp.execute({
+                  title = "Pin Main",
+                  filter = "tinymist",
+                  command = "tinymist.pinMain",
+                  arguments = { buf_name },
+                })
+                LazyVim.info("Tinymist: Pinned " .. file_name)
+              end,
+              desc = "Pin main file",
+            },
+          },
+          single_file_support = true, -- Fixes LSP attachment in non-Git directories
+          settings = {
+            formatterMode = "typstyle",
+            formatterProseWrap = true, -- wrap lines in content mode
+            formatterPrintWidth = 80, -- limit line length to 80 if possible
+            formatterIndentSize = 4, -- indentation width
+          },
+        },
+      },
       setup = {
         eslint = function()
           require("snacks.util").lsp.on(function(_, client)
