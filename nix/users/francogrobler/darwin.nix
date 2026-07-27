50dcf51d2ded879aae582bcef21072ff7c4148da
Franco Grobler <franco@grobler.fyi>
2026-07-27 18:17:35 +0200

    chore(nix): cleanup and migration
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nix/users/francogrobler/darwin.nix
+++ nix/users/francogrobler/darwin.nix
@@ -7,7 +7,6 @@
       "cocoapods"
       "colima"
       "container"
-      "gemini-cli"
       "llvm"
       "mas"
       "mole"
@@ -17,6 +16,7 @@
       # "alacritty"
       # "anydesk"
       "balenaetcher"
+      "claude-code@latest"
       "drawio"
       "figma"
       # "flutter"
@@ -52,7 +52,7 @@
     };
     onActivation = {
       autoUpdate = false;
-      cleanup = "uninstall";
+      # cleanup = "uninstall";
       upgrade = true;
     };
   };
