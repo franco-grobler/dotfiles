4a92006a8c1750a69e461b7e8df8207dfda58268
Franco Grobler <franco@grobler.fyi>
2026-09-15 22:42:42 +0200

    chore(nix): cleanup
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nix/modules/home/vcs/git-cliff/default.nix
+++ nix/modules/home/vcs/git-cliff/default.nix
@@ -1,5 +1,6 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home.packages = [ pkgs.git-cliff ];
+  home.sessionVariables.GIT_CLIFF_CONFIG = "${config.xdg.configHome}/git-cliff/cliff.toml";
   xdg.configFile."git-cliff/cliff.toml".source = ./cliff.toml;
 }
