39645771ed502a5f32355b6c0c8d663e7f0cbe0c
Franco Grobler <franco@grobler.fyi>
2026-09-15 21:36:14 +0200

    refactor(nix): macos defaults
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nix/modules/darwin/system/macos-defaults.nix
+++ nix/modules/darwin/system/macos-defaults.nix
@@ -1,46 +1,50 @@
 {
-  system.defaults = {
-    finder = {
-      AppleShowAllExtensions = true;
-      FXPreferredViewStyle = "Nlsv";
-      FXRemoveOldTrashItems = true;
-      NewWindowTarget = "iCloud Drive";
-      ShowExternalHardDrivesOnDesktop = true;
-      ShowHardDrivesOnDesktop = false;
-      ShowMountedServersOnDesktop = true;
-      ShowPathbar = true;
-      ShowStatusBar = true;
-    };
-    iCal = {
-      "TimeZone support enabled" = true;
-      "first day of week" = "Monday";
-    };
-    menuExtraClock = {
-      FlashDateSeparators = true;
-      IsAnalog = false;
-      Show24Hour = true;
-      ShowDate = 0;
-      ShowDayOfMonth = true;
-      ShowDayOfWeek = true;
-      ShowSeconds = true;
-    };
-    screencapture = {
-      location = "~/Desktop/Screenshots";
-      target = "file";
-    };
-    trackpad = {
-      Clicking = true;
-      FirstClickThreshold = 1;
-      SecondClickThreshold = 1;
-      TrackpadRightClick = true;
-      TrackpadThreeFingerDrag = true;
-    };
-  };
+  system = {
+    defaults = {
+      finder = {
 
-  system.keyboard = {
-    enableKeyMapping = false;
-    swapLeftCtrlAndFn = false;
-  };
+        AppleShowAllExtensions = true;
+        FXPreferredViewStyle = "Nlsv";
+        FXRemoveOldTrashItems = true;
+        NewWindowTarget = "iCloud Drive";
+        ShowExternalHardDrivesOnDesktop = true;
+        ShowHardDrivesOnDesktop = false;
+        ShowMountedServersOnDesktop = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+      };
 
-  system.startup.chime = false;
+      iCal = {
+        "TimeZone support enabled" = true;
+        "first day of week" = "Monday";
+      };
+      menuExtraClock = {
+        FlashDateSeparators = true;
+        IsAnalog = false;
+        Show24Hour = true;
+        ShowDate = 0;
+        ShowDayOfMonth = true;
+        ShowDayOfWeek = true;
+        ShowSeconds = true;
+      };
+      screencapture = {
+        location = "~/Desktop/Screenshots";
+        target = "file";
+      };
+      trackpad = {
+        Clicking = true;
+        FirstClickThreshold = 1;
+        SecondClickThreshold = 1;
+        TrackpadRightClick = true;
+        TrackpadThreeFingerDrag = true;
+      };
+    };
+
+    keyboard = {
+      enableKeyMapping = false;
+      swapLeftCtrlAndFn = false;
+    };
+
+    startup.chime = false;
+  };
 }
