04b98a68d8c3cd8948cc5cc848ffabb9cd8a128b
Franco Grobler <franco@grobler.fyi>
2026-09-17 21:10:50 +0200

    fix(nix): opencode config file
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

--- nix/modules/home/ai/opencode/default.nix
+++ nix/modules/home/ai/opencode/default.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home.packages = [ pkgs.opencode ];
-  xdg.configFile."opencode/opencode.jsonc".source = ./opencode.jsonc;
+  xdg.configFile."opencode/opencode.jsonc".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.dotfiles.root}/nix/modules/home/ai/opencode/opencode.jsonc";
+
 }
